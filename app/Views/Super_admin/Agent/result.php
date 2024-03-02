5099c88f805a049ab8062ddd06210f633ad8caca
imranertaza <imranertaza12@gmail.com>
2024-03-02 12:31:26 +0600

    sume update

--- app/Views/Super_admin/Agent/result.php
+++ app/Views/Super_admin/Agent/result.php
@@ -5,12 +5,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Delivery boy</h1>
+                    <h1>Agent</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="<?php echo base_url('super_admin/shops')?>">Home</a></li>
-                        <li class="breadcrumb-item active">Delivery boy</li>
+                        <li class="breadcrumb-item active">Agent</li>
                     </ol>
                 </div>
             </div>
@@ -25,8 +25,8 @@
 
                     <div class="card">
                         <div class="card-header">
-                            <a href="<?php echo base_url('super_admin/delivery_boy_create');?>" class="btn btn-xs btn-primary w-25 float-right">Create</a>
-                            <h3 class="card-title">Delivery boy list</h3>
+                            <a href="<?php echo base_url('super_admin/agent_create');?>" class="btn btn-xs btn-primary w-25 float-right">Create</a>
+                            <h3 class="card-title">Agent list</h3>
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
@@ -34,14 +34,14 @@
                                 <?php echo  isset(newSession()->message) ? newSession()->message :''; ?>
                             </div>
                             <div class="col-md-12">
-                                <form action="<?php echo base_url('super_admin/delivery_boy_filter')?>" method="post">
+                                <form action="<?php echo base_url('super_admin/agent_filter')?>" method="post">
                                     <div class="row">
 
                                         <div class="form-group col-md-2">
                                             <label for="varchar">Division </label>
                                             <select class="form-control" name="division" onchange="viewdistrict(this.value)">
                                                 <option value="">Please Select</option>
-                                                <?php echo divisionView(); ?>
+                                                <?php echo divisionView($address->division); ?>
                                             </select>
                                         </div>
 
@@ -49,7 +49,7 @@
                                             <label for="varchar">District</label>
                                             <select class="form-control" name="district" onchange="viewupazila(this.value)" id="district" required>
                                                 <option value="">Please Select</option>
-                                                <?php echo districtselect(); ?>
+                                                <?php echo districtselect($address->zila,$address->division); ?>
                                             </select>
 
 
@@ -59,25 +59,26 @@
                                             <label for="varchar">Upazila </label>
                                             <select class="form-control" name="upazila" id="upazila" required>
                                                 <option value="">Please Select</option>
-                                                <?php echo upazilaselect(); ?>
+                                                <?php echo upazilaselect($address->upazila,$address->zila); ?>
                                             </select>
                                         </div>
 
                                         <div class="form-group col-md-2">
                                             <label for="varchar">Pourashava/Union</label>
-                                            <select class="form-control" name="pourashava">
+                                            <select class="form-control" name="pourashava" required>
                                                 <option value="">Please Select</option>
-                                                <?php echo pourashavaUnion(); ?>
+                                                <?php echo pourashavaUnion($address->pourashava); ?>
                                             </select>
                                         </div>
 
                                         <div class="form-group col-md-2">
                                             <label for="varchar">Ward</label>
-                                            <select class="form-control" name="ward">
+                                            <select class="form-control" name="ward" required>
                                                 <option value="">Please Select</option>
-                                                <?php echo wardView(); ?>
+                                                <?php echo wardView($address->ward); ?>
                                             </select>
                                         </div>
+
                                         <div class="form-group col-md-2">
                                             <button type="submit" class="btn btn-primary "  style="margin-top: 30px; width:100%;">Filter </button>
                                         </div>
@@ -85,42 +86,39 @@
                                     </div>
                                 </form>
                             </div>
-
                             <table id="example1" class="table table-bordered table-striped">
                                 <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Name</th>
-                                    <th>Phone</th>
-                                    <th>Balance</th>
-                                    <th>Action</th>
-                                </tr>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                        <th>Action</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                <?php  foreach ($deliveryboy as $val){ ?>
+                                <?php  foreach ($agent as $val){ ?>
                                     <tr>
-                                        <td width="10"><?php echo $val->delivery_boy_id;?></td>
-                                        <td><?php echo $val->name ?></td>
+                                        <td width="20"><?php echo $val->agent_id;?></td>
+                                        <td><?php echo $val->agent_name ?></td>
+                                        <td><?php echo $val->email ?></td>
                                         <td><?php echo $val->mobile ?></td>
-                                        <td><?php echo showWithCurrencySymbol($val->balance) ?></td>
-                                        <td width="300px">
-                                            <a href="<?php echo base_url('super_admin/delivery_boy_order/'.$val->delivery_boy_id);?>" class="btn btn-xs btn-info ">Order</a>
-                                            <a href="<?php echo base_url('super_admin/delivery_boy_commission/'.$val->delivery_boy_id);?>" class="btn btn-xs btn-primary ">Commission</a>
-                                            <a href="<?php echo base_url('super_admin/delivery_boy_ledger/'.$val->delivery_boy_id);?>" class="btn btn-xs btn-success ">Ledger</a>
-                                            <a href="<?php echo base_url('super_admin/delivery_boy_update/'.$val->delivery_boy_id);?>" class="btn btn-xs btn-warning ">Update</a>
-                                            <a href="<?php echo base_url('super_admin/delivery_boy_delete/'.$val->delivery_boy_id);?>" onclick="javasciprt: return confirm('Are You Sure ?')" class="btn btn-xs btn-danger">Delete</a>
+                                        <td width="200px">
+                                            <a href="<?php echo base_url('super_admin/agent_commission/'.$val->agent_id);?>" class="btn btn-xs btn-primary ">Shop Commission</a>
+                                            <a href="<?php echo base_url('super_admin/agent_update/'.$val->agent_id);?>" class="btn btn-xs btn-warning ">Update</a>
+                                            <a href="<?php echo base_url('super_admin/agent_delete/'.$val->agent_id);?>" onclick="javasciprt: return confirm('Are You Sure ?')" class="btn btn-xs btn-danger">Delete</a>
                                         </td>
                                     </tr>
                                 <?php } ?>
                                 </tbody>
                                 <tfoot>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Name</th>
-                                    <th>Phone</th>
-                                    <th>Balance</th>
-                                    <th>Action</th>
-                                </tr>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                        <th>Action</th>
+                                    </tr>
                                 </tfoot>
                             </table>
                         </div>
